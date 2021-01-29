eded8eb14e891e270a6e3a2b14239274a2fa17a3
Igor Malyga <igormalyga@gmail.com>
2021-01-29 16:55:21 +0200

    Update Category model

--- server/expenses/models.py
+++ server/expenses/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    created_by = models.ForeignKey(
+        'accounts.Profile', on_delete=models.CASCADE, related_name='categories')
 
     class Meta:
         verbose_name_plural = "Categories"
