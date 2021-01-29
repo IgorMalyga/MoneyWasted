6fa9c6527de95ec239a5049272585c310b83bb0a
Igor Malyga <igormalyga@gmail.com>
2021-01-29 17:43:53 +0200

    Add cheque model

--- server/expenses/models.py
+++ server/expenses/models.py
@@ -11,3 +11,11 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Cheque(models.Model):
+    created_by = models.ForeignKey(
+        'accounts.Profile', on_delete=models.CASCADE, related_name='cheques')
+    category = models.ForeignKey(
+        'expenses.Category', on_delete=models.CASCADE, related_name='cheques')
+    amount = models.FloatField(default=0)
