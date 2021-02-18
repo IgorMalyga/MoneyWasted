1e49b4731d17b040f1b0f6dfc605365a4172b324
Igor Malyga <igormalyga@gmail.com>
2021-02-18 19:11:12 +0200

    Add custom user manager

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -1,14 +1,49 @@
 import datetime
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
 from django.db import models
 
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
 class User(AbstractUser):
+    username = None
     email = models.EmailField(unique=True)
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
 
 class Profile(models.Model):
