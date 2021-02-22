293771e07a9eb9f687283a374cbc6ec5211edac9
Igor Malyga <igormalyga@gmail.com>
2021-02-22 17:34:55 +0200

    Add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import Landing from './pages/Landing';
+import { Dashboard } from './pages/dashboard';
 
+import * as routes from './constants/routes';
 import './App.css';
 
-const App = () => <Landing />;
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path={routes.DASHBOARD}>
+        <Dashboard />
+      </Route>
+      <Route path={routes.LANDING}>
+        <Landing />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
