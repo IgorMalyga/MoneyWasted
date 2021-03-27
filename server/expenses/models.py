39581264ccb95b784b8efa940ddf83dd54e6360e
Igor Malyga <igormalyga@gmail.com>
2021-03-27 14:00:01 +0200

    Add wallet model

--- server/expenses/models.py
+++ server/expenses/models.py
@@ -1,4 +1,24 @@
+import datetime
+
 from django.db import models
+
+
+class Wallet(models.Model):
+    user = models.ForeignKey(
+        'accounts.User', on_delete=models.CASCADE, related_name='wallets')
+    current_money = models.FloatField(default=0)
+    payday_date = models.DateField(blank=True, null=True)
+
+    @property
+    def days_to_payday_left(self):
+        return abs((self.payday_date-datetime.datetime.today().date()).days)
+
+    @property
+    def money_per_day(self):
+        return self.current_money / self.days_to_payday_left
+
+    def __str__(self):
+        return "%s %s" % (self.id, self.user.email)
 
 
 # class Category(models.Model):
