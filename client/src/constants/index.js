2587318e9cfc45570b9bfe280792fe971222a1bb
Igor Malyga <igormalyga@gmail.com>
2021-04-10 17:02:04 +0300

    Display user's wallets on dashboard

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,4 +1,10 @@
 export const BASE_BACKEND_URL = 'http://localhost:8000/api/';
+
+// auth related endpoints
 export const LOGIN = 'accounts/login/';
 export const VERIFY_TOKEN = 'accounts/verify-token/';
 export const SIGNUP = 'accounts/signup/';
+
+// expenses related endpoints
+
+export const WALLETS = 'expenses/wallets/';
--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+import { axiosWrapp } from '../../utils';
 import { withAuth } from '../../hocs';
+
+import { WALLETS } from '../../constants/index';
 
 /* eslint-disable */
 const Dashboard = (props) => {
-  return <h1>Welcome to dashboard</h1>;
+  const [wallets, setWallets] = useState(null);
+  const [errors, setErrors] = useState([]);
+
+  useEffect(() => {
+    axiosWrapp()
+      .get(WALLETS)
+      .catch((error) => {
+        setErrors(error.response);
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.data.length) {
+          setWallets(response.data);
+        }
+      });
+  }, []);
+
+  return (
+    <>
+      {wallets ? (
+        <div>
+          {wallets.map((wallet) => {
+            return <span>{wallet.currency}</span>;
+          })}
+        </div>
+      ) : (
+        <h1>You have not wallets yet :(</h1>
+      )}
+    </>
+  );
 };
 
 export default withAuth(Dashboard);
